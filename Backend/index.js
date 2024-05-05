5877836f5a6bbfb1962654e3b8f23f96f9a0422b
Jeewantha De Silva <100590888+IT21228612@users.noreply.github.com>
2024-05-05 11:36:54 +0530

    updated index.js

--- Backend/index.js
+++ Backend/index.js
@@ -6,7 +6,10 @@ import userRoute from './routes/userRoute.js';
 import authRoute  from './routes/authorizationRoute.js';
 import cookieParser from 'cookie-parser';
 
-const MONGO_URI = "mongodb+srv://jeewa2000:itpIT2080@mernapp.4rrg7bl.mongodb.net/?retryWrites=true&w=majority&appName=MERNapp"
+// Load environment variables from .env file
+dotenv.config();
+
+const port = process.env.PORT;
 
 
 dotenv.config();
@@ -24,8 +27,8 @@ mongoose.connect(
 const app = express();
 app.use(express.json());
 app.use(cookieParser());
-app.listen(3000, () =>{
-    console.log("Server is running on port 3000!!")
+app.listen(port, () =>{
+    console.log(`Server is running on port ${port}`);
 });
 app.use('/api/user', userRoute);
 app.use('/api/auth', authRoute);

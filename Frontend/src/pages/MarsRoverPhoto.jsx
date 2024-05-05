730d3c6bbed6850502c5c83eebb512dba166102e
Jeewantha De Silva <100590888+IT21228612@users.noreply.github.com>
2024-05-05 10:52:18 +0530

    sol date placeholder updated

--- Frontend/src/pages/MarsRoverPhoto.jsx
+++ Frontend/src/pages/MarsRoverPhoto.jsx
@@ -95,7 +95,7 @@ export default function MarsRoverPhoto() {
                   min="999"
                   max="1500"
                   className="mt-2 px-3 py-2 border border-gray-300 rounded-md w-full focus:outline-none focus:border-blue-500"
-                  placeholder="Image Count"
+                  placeholder="Enter sol date"
                   value={startDate}
                   onChange={(e) => setStartDate(e.target.value)}
                 />
